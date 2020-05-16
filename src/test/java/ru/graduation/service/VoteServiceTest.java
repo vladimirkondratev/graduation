93e450a44870f8653d5d785d4789b6056e53b8c5
vladimirkondratev <kondrvl@yandex.ru>
2020-05-16 09:58:17 +0300

    add VoteServiceTest

---
+++ src/test/java/ru/graduation/service/VoteServiceTest.java
@@ -0,0 +1,30 @@
+package ru.graduation.service;
+
+import org.junit.jupiter.api.Test;
+
+class VoteServiceTest {
+
+    @Test
+    void doVote() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getForUserAndDate() {
+    }
+
+    @Test
+    void getAllForUser() {
+    }
+
+    @Test
+    void getAllForRestaurant() {
+    }
+}

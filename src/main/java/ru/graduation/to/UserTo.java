a35d84b274dae1b97367d055ec196ae58460d954
vladimirkondratev <kondrvl@yandex.ru>
2020-05-27 18:34:52 +0300

    add serializable to UserTo

--- src/main/java/ru/graduation/to/UserTo.java
+++ src/main/java/ru/graduation/to/UserTo.java
@@ -3,8 +3,11 @@ package ru.graduation.to;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 
-public class UserTo extends BaseTo {
+public class UserTo extends BaseTo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @NotBlank
     @Size(min = 2, max = 100)

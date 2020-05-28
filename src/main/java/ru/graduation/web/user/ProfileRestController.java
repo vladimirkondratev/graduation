cf4768cc52157e5419ac85f479ac0edb211f82ca
vladimirkondratev <kondrvl@yandex.ru>
2020-05-28 11:37:32 +0300

    @AuthenticationPrincipal

--- src/main/java/ru/graduation/web/user/ProfileRestController.java
+++ src/main/java/ru/graduation/web/user/ProfileRestController.java
@@ -3,15 +3,15 @@ package ru.graduation.web.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.graduation.AuthorizedUser;
 import ru.graduation.model.User;
 import ru.graduation.to.UserTo;
 
 import javax.validation.Valid;
 import java.net.URI;
-
-import static ru.graduation.web.SecurityUtil.authUserId;
 
 @RestController
 @RequestMapping(ProfileRestController.REST_URL)
@@ -32,18 +32,18 @@ public class ProfileRestController extends AbstractUserController {
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@Valid @RequestBody UserTo userTo) {
-        super.update(userTo, authUserId());
+    public void update(@Valid @RequestBody UserTo userTo, @AuthenticationPrincipal AuthorizedUser authUser) {
+        super.update(userTo, authUser.getId());
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public User get() {
-        return super.get(authUserId());
+    public User get(@AuthenticationPrincipal AuthorizedUser authUser) {
+        return super.get(authUser.getId());
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete() {
-        super.delete(authUserId());
+    public void delete(@AuthenticationPrincipal AuthorizedUser authUser) {
+        super.delete(authUser.getId());
     }
 }
--- src/main/java/ru/graduation/web/vote/VoteRestController.java
+++ src/main/java/ru/graduation/web/vote/VoteRestController.java
@@ -6,16 +6,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.graduation.AuthorizedUser;
 import ru.graduation.model.Vote;
 import ru.graduation.service.VoteService;
 
 import java.net.URI;
 import java.time.LocalDate;
 import java.util.List;
-
-import static ru.graduation.web.SecurityUtil.authUserId;
 
 @RestController
 @RequestMapping(value = VoteRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,9 +32,9 @@ public class VoteRestController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Vote> vote(@RequestParam int restaurantId) {
+    public ResponseEntity<Vote> vote(@RequestParam int restaurantId, @AuthenticationPrincipal AuthorizedUser authUser) {
         log.info("vote for restaurant: {}", restaurantId);
-        Vote created = voteService.doVote(authUserId(), restaurantId);
+        Vote created = voteService.doVote(authUser.getId(), restaurantId);
         if (created != null) {
             URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                     .path(REST_URL + "/{id}")
